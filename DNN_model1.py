ec7f231433da5db1d04347c9edc58ee378750475
TyongKim <tyong.kim0117@gmail.com>
2022-11-12 16:40:30 -0500

    Update readme

---
+++ DNN_model1.py
@@ -0,0 +1,62 @@
+"""
+The code predicts the mean, standard deviation, and correlation coefficients of
+the EDP residuals by using three deep neural network (DNN) models.
+
+The DNN model is already trained by using a seismic demand database that is 
+constructed through "Construct_SDDB.py"
+
+The details are found in Section 3 of the following reference.
+Kang, C.1, Kim, T.1, Kwon, O., and Song, J. (2023). Deep neural network-based 
+regional seismic loss assessment considering correlation between EDP residual 
+of building structures, Earthquake Engineering and Structural Dynamics, (Accepted) 
+
+
+Developed by Taeyong Kim from the University of Toronto
+Nov 12, 2022
+"""
+
+# Import libraries
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+#%% Import period and damping values
+Stiffness = np.load('stiffness.npy')
+g = 9.81 # gravitational acceleration
+period = 2*np.pi*np.sqrt(1/g/Stiffness);
+damping = np.asarray([0.00, 0.001, 0.005, 0.010, 0.015,
+                      0.02, 0.025, 0.03, 0.035, 0.04, 
+                      0.05, 0.07, 0.10, 0.12, 0.15, 
+                      0.20, 0.25, 0.30, 0.35, 0.40]) # 20 steps
+
+#%% Import DNN models
+
+NN_model_mean =  keras.models.load_model('DNN_Model1_mean_final.h5')
+NN_model_std =  keras.models.load_model('DNN_Model1_std_final.h5')
+NN_model_corr = keras.models.load_model('DNN_Model1_corr_final.h5')
+NN_model_corr.summary()
+
+#%% Target structural systems (we asuume a certain situation)
+# ith structure
+period_1_i = 0.32; period_i_k = 0.095; damping_k_i = 0.02
+# jth structure
+period_1_j = 3.377; period_j_l = 0.7; damping_l_j = 0.05
+
+Input_DNN = np.array([[np.log(period_1_i), (period_1_i-period_i_k)/period_1_i, damping_k_i],
+                      [np.log(period_1_j), (period_1_j-period_j_l)/period_1_j, damping_l_j]])
+
+Input_corr = np.array([[np.log(period_1_i), (period_1_i-period_i_k)/period_1_i, damping_k_i,
+                        np.log(period_1_j), (period_1_j-period_j_l)/period_1_j, damping_l_j]])                       
+
+#%% Predict thorugh DNN models
+# Mean
+Output_mean = NN_model_mean.predict(Input_DNN)
+Output_mean = np.exp(Output_mean)
+
+# standard deviation
+Output_std = NN_model_std.predict(Input_DNN)
+Output_std = np.exp(Output_std)
+
+# Correlation coefficient
+Output_corr = NN_model_corr.predict(Input_corr)
+
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # EDP_residual
 
 ## Software introduction
-This software predicts the standard deviation and correlation of EDP residuals using deep neural network (DNN) models. This page shares codes to construct database and develop DNN models. Note that due to the storage issue, the constructued database would not be shared but the trained DNN models can be downloaded.
+This software predicts the standard deviation and correlation of EDP residuals using deep neural network (DNN) models. This page shares codes to construct a database and develop DNN models. Note that due to the storage issue, the constructed database would not be shared but the trained DNN models can be downloaded.
 
 ## Developers
 Developed by Chulyoung Kang (cykang@kaeri.re.kr), Taeyong Kim (tyong.kim@mail.utoronto.ca), Oh-Sung Kwon (os.kwon@utoronto.ca), and Junho Song (junhosong@snu.ac.kr)
@@ -18,7 +18,13 @@ The paper was invited to a special issue “AI and Data-driven Methods in Earthq
 Python 3 with Numpy version '1.20.2', Scipy version '1.7.3', Tensorflow version '2.9.1'
 
 
-OpenSees version '3.3.0'
+OpenSees version '3.3.0'; https://opensees.berkeley.edu/OpenSees/user/download.php
 
 ## File description
-1. Construct_SDDB: 
+1. Construct_SDDB.py: This code helps you construct a seismic demand database of linear single-degree-of-freedom (SDOF) systems having different stiffness (or period) and damping coefficients. To run this code, OpenSees is required. Moreover, a set of ground motions needs to be downloaded from the NGA-WEST database.
+
+2. DNN_model1.py: This code helps users how to use the DNN model developed in Section 3 of the authors' paper. The mean, standard deviation, and correlation coefficients of the EDP residulas are predicted from the DNN models.
+
+3. DNN_model2.py: This code helps users how to use the DNN model developed in Section 4 of the authors' paper. The standard deviation and correlation coefficients of the EDP resiudals are predicted from the DNN models.
+
+4. Numerical_example.py: Thiis code copmares the predicted EDP residuals from the DNN model 1 and DNN model 2. Based on this code, users can easily write their own code for their projects or research.
